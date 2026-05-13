67ab7c742463d3200b75a17d0b5247442782143b
yusufsalaam101 <yusufsalaam101@gmail.com>
2026-05-13 14:18:01 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
-import base44 from "@base44/vite-plugin"
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', 
-  logLevel: 'error',
-  plugins: [
-    base44({
-      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
-      hmrNotifier: true,
-      navigationNotifier: true,
-      analyticsTracker: true,
-      visualEditAgent: true
-    }),
-    react(),
-  ]
-});
+  plugins: [react()],
+  base: './'
+})
