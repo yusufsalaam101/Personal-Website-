17cb4aa1fe933abb74a89cc33289c877aeeced93
yusufsalaam101 <yusufsalaam101@gmail.com>
2026-05-13 14:21:53 +0200

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh',
+      fontFamily: 'sans-serif' 
+    }}>
+      <h1>Yusuf's Portfolio is Live! 🚀</h1>
+    </div>
+  )
+}
